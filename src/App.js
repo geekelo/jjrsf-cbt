8eaa4d744ec978f3d17e8b33d907c092e7990856
nicojoy-gif <orjionyinyechi080@gmail.com>
2025-03-13 21:03:52 +0100

    updated app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
 import "./App.css";
 import Home from "./components/Home";
